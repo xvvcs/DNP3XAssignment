f9ec96712c17c633148b8ecdf56ad159bb9905ea
Jakub Abramek <kubrama04@gmail.com>
2024-09-08 19:16:17 +0200

    Finished Subforum feature

--- Server/InMemoryRepositories/SubForumInMemoryRepository.cs
+++ Server/InMemoryRepositories/SubForumInMemoryRepository.cs
@@ -2,7 +2,7 @@
 using Entities;
 using RepositoryContracts;
 
-public class SubForumInMemoryRepository
+public class SubForumInMemoryRepository: ISubForumRepository
 {
     private List<SubForum> subForums = new List<SubForum>();
 
@@ -15,5 +15,49 @@ public class SubForumInMemoryRepository
         }
 
         return exisitingSubForum;
+    }
+
+    public SubForum FindSubForumById(int id)
+    {
+        SubForum? exisitingSubForum = subForums.FirstOrDefault(s => s.Id == id);
+        if (exisitingSubForum is null)
+        {
+            throw new InvalidOperationException($"SubForum with ID '{id}' not found.");
+        }
+
+        return exisitingSubForum;
+    }
+
+    public Task<SubForum> AddAsync(SubForum subForum)
+    {
+        subForum.Id = subForums.Any()
+           ? subForums.Max(s => s.Id) + 1
+            : 1;
+        subForums.Add(subForum);
+        return Task.FromResult(subForum);
+    }
+
+    public Task UpdateAsync(SubForum subForum)
+    {
+      subForums.Remove(FindSubForum(subForum));
+      subForums.Add(subForum);
+      
+      return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        subForums.Remove(FindSubForumById(id));
+        return Task.CompletedTask;
+    }
+    
+    public IQueryable<SubForum> GetManyAsync()
+    {
+        return subForums.AsQueryable();
+    }
+
+    public Task<SubForum> GetSingleAsync(int id)
+    {
+        return Task.FromResult(FindSubForumById(id));
     }
 }
