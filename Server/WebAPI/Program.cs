820bf90b2d254208052596b3fa3d827294eade50
xvvcs <maciek.m2004@wp.pl>
2024-09-26 20:32:42 +0200

    TODOs

--- Server/WebAPI/Program.cs
+++ Server/WebAPI/Program.cs
@@ -3,6 +3,10 @@ using RepositoryContracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//TODO: ADD BUSINESS LOGIC TO THE APPLICATION
+//TODO: ADD VALIDATION FOR CREATING USERS
+//TODO: FIX LIKES AND DISLIKES
+
 // Add services to the container.
 
 builder.Services.AddControllers();
