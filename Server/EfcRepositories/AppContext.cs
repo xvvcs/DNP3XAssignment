361205716f8503c3466d4f37c474bda3f1891158
Richardzik <kubrama04@gmail.com>
2024-11-20 16:30:34 +0100

    efc initial setup folder

---
+++ Server/EfcRepositories/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfcRepositories;
+
+public class AppContext : DbContext
+{
+    public DbSet<Post> Posts => Set<Post>(); 
+    public DbSet<User> Users => Set<User>(); 
+    public DbSet<Comment> Comments => Set<Comment>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=app.db");
+    }
+}
